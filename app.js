568f86c81aa50a261076b8261dbc01c0b4519fed
Ashita Bansal <ashita1304@gmail.com>
2022-06-22 16:43:37 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,76 @@
+//jshint esversion:6
+
+const express=require("express");
+const bodyParser=require("body-parser");
+const https=require("https");
+const request=require("request");
+
+const app=express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname+"/signup.html");
+})
+
+app.post("/",function(req,res){
+    
+    const firstName=req.body.fname;
+    const lastName=req.body.lname;
+    const eMail=req.body.email;
+
+    const data={
+        members:[
+            {
+                email_address:eMail,
+                status:"subscribed",
+                merge_fields:{
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            
+            }
+        ]
+    };
+
+    const jsonData=JSON.stringify(data);
+
+    const url="https://us18.api.mailchimp.com/3.0/lists/111952cc2e";
+
+    const options={
+        method:"POST",
+        auth:"ashita1:5bdae96671aca6f56312b2d0b0c9bbd3-us18"
+    }
+
+    const request=https.request(url,options,function(response){
+
+        if(response.statusCode===200){
+            res.sendFile(__dirname+"/success.html")
+        }else{
+            res.sendFile(__dirname+"/failure.html")
+        }
+
+        response.on("data",function(data){
+            console.log(JSON.parse(data))
+        })
+    })
+
+    request.write(jsonData);
+
+    request.end();
+
+    
+});
+
+app.post("/failure",function(req,res){
+    res.redirect("/");
+});
+
+app.listen(process.env.PORT || 3000,function(){
+    console.log("server is running on port 3000");
+});
+
+
+//api key for mailchimp:5bdae96671aca6f56312b2d0b0c9bbd3-us18
+//list id :111952cc2e
